c5f487e5fb2875ae0317d08961e4fef16d34418f
akhmadeev <akhmadeev.a.r@mail.ru>
2021-03-23 14:55:30 +0300

    style

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk';
 import allRudecesrs from "./store/reducers";
-import App from './components/app/App';
+import App from './components/app/App.js';
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
