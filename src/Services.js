836dbf703c62971a62e063b6a20b49d4f8e4183c
akhmadeev <akhmadeev.a.r@mail.ru>
2021-03-24 12:04:16 +0300

    editing

--- src/Services.js
+++ src/Services.js
@@ -2,16 +2,16 @@ export default class Services {
   baseUrl = 'https://front-test.beta.aviasales.ru';
 
   getResourse = async (url, option) => {
-    const res = await fetch(url, option);
+    const res = await fetch(`${this.baseUrl}${url}`, option);
     if (!res.ok) throw new Error(res.status);
     const body = res.json();
     return body;
   };
 
-  requestApi = () => this.getResourse(`${this.baseUrl}/search`);
+  requestApi = () => this.getResourse(`/search`);
 
   requestTickets = () =>
-    this.getResourse(`${this.baseUrl}/tickets?searchId=${localStorage.getItem('searchId')}`).catch(
+    this.getResourse(`/tickets?searchId=${localStorage.getItem('searchId')}`).catch(
       () => this.requestTickets()
     );
 }
